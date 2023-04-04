c866fd90833178d69e24ecd27d4b8b6c7487d54d
uvsq22102970 <114027150+uvsq22102970@users.noreply.github.com>
2023-04-04 11:53:52 +0200

    Update Vrai_2048.py

--- Vrai_2048.py
+++ Vrai_2048.py
@@ -45,3 +45,49 @@ label.grid()
 label.config(text="2048", bg="gold") 
 # Lancement de la boucle principale
 racine.mainloop()
+
+
+#definir les actions
+actions = ["up", "down","left","right","restart","exit"]
+
+
+#definir le jeu
+def jeu():
+   if action == "restart":
+       return "Init"
+   if action == "exit":
+       return " exit"
+       if le joueur a gagné:
+           return "Win"
+       if le joueur a perdu:
+           return "Gameover"
+
+
+#definir les mouvements
+def move(ligne):
+   ###avec la fonction serre on va presser les tuiles
+   def serre(ligne):
+       ###pour les tuiles avec un nombre
+       new_row = [ i for i in ligne if i != 0]
+       ###pour les tuiles sans valeur
+       new_row += [ 0 for i in range(len(ligne) - len(new_row))]
+       return new_row
+   ###avec la fonction fusion on va fusionner les tuiles adjacents ayant les memes nombres
+   def fusion(ligne):
+       pair = False
+       new_row = []
+       for i in range(len(ligne)):
+           if pair:
+               ###ici on definit l'action: 2 tuiles qui fusionnent = multiplie leur valeur par 2
+               new_row.append(2 * ligne[i])
+           else:
+               ###si on fusionne 2 tuiles sur une ligne, alors on doit mettre une tuile vide(0) dans la ligne
+               if i + 1 < len(ligne) and ligne[i] == ligne[i + 1]:
+                   pair = True
+                   new_row.append(0)
+               ###si les 2 tuiles ne peuvent pas fusionner alors on doit ajouter ces elements a la nouvelle liste
+               else:
+                    new_row.append(ligne{i})
+       ###on s'assure ue les fusion ne deforme pas la matrice
+       assert len(new_row) == len(ligne)
+       return new_row
